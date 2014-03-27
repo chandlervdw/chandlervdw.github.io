8e87e2b1e23da9791b5e9cb3758054a022dcc7d2
Chandler Van De Water <chandler.vdw@gmail.com>
2014-03-27 15:36:08 -0400

    added Dribbbox by Benjamin DeCock

---
+++ dribbble/config.js
@@ -0,0 +1,10 @@
+// Howdy! This is your website's configuration file.
+// Just replace my personal info with yours, save, and you're done :)
+
+dribbbox.config = {
+
+  dribbble_username : "chandlervdw",
+  short_description : "Illustrator, User Interface Designer and Frontend Developer.",
+  email_address     : "howdy@chandlervdw.com"
+
+}
